bd138e27525c0dd5b8b2fd7a3831f326b31be9d2
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2026-01-05 10:35:46 +0300

    added  place holder next to the button.

---
+++ script.js
@@ -0,0 +1,4 @@
+const input = document.getElementById('habitInput');
+const button = document.getElementById('trackButton');
+
+button.addEventListener("click", function() {console.log(input.value);});
