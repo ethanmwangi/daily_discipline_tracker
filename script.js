b14f10c4f4a243e4148bf0b620d25ff8f339ecdd
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2026-01-12 15:16:22 +0300

    added a second habit

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-const input = document.getElementById('habitInput');
-const button = document.getElementById('trackButton');
+const input = document.querySelector('#habit-input');
+const addButton = document.querySelector('#add-btn');
+const habitList = document.querySelector('#habit-list');
+
 
 button.addEventListener("click", function() {console.log(input.value);});
+addButton.addEventListener('click', function() {
+    const habitText = input.value;
+    
+    if (habitText === '') return; // ignore empty input
+
+    const li = document.createElement('li');
+    li.textContent = habitText;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    li.prepend(checkbox);
+
+    habitList.appendChild(li);
+
+    input.value = '';
+});
+
+button.addEventListener("click", function() {console.log(input.value);});
+addButton.addEventListener('click', function() {
+    const habitText = input.value;
+
+    if (habitText === '') return; // ignore empty input
+
+    const li = document.createElement('li');
+    li.textContent = habitText;
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    li.prepend(checkbox);
+    habitList.appendChild(li);
+    input.value = '';
+});
